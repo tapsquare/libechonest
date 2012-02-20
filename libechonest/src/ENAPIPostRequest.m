888ebafec6591b8acbd9547ad620e1c5f55d87aa
Andrew Lenards <lenards@iplantcollaborative.org>
2012-02-19 21:20:52 -0700

    Initial replaced of ASIFormDataRequest.

--- libechonest/src/ENAPIPostRequest.m
+++ libechonest/src/ENAPIPostRequest.m
@@ -29,53 +29,65 @@
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "ENAPIPostRequest.h"
-#import "asi-http-request/ASIFormDataRequest.h"
-#import "asi-http-request/ASIProgressDelegate.h"
 #import "ENAPI.h"
 #import "ENAPI_utils.h"
 #import "ENAPIPostRequest.h"
 #import "NSObject+JSON.h"
 
 @interface ENAPIPostRequest() 
-@property (retain) ASIFormDataRequest *request;
+@property (retain) NSMutableURLRequest *request;
 @property (retain) NSDictionary *_responseDict;
-@property (nonatomic, assign) unsigned long long uploadFileSize;
-@property (nonatomic, assign) unsigned long long uploadBytesSent;
+@property (retain) NSURLConnection *connection;
+@property (retain) NSHTTPURLResponse *internalResponse;
+@property (retain) NSMutableData *receivedData;
+@property (retain) NSString *responseString;
+@property (retain) NSError *internalError;
+@property (retain) NSMutableDictionary *postValues;
+@property (retain) NSMutableDictionary *fileData;
+
+-(void)_preparePostBody;
+-(void)_requestFailed;
+-(void)_requestFinished;
 @end
 
 @implementation ENAPIPostRequest
-@synthesize delegate, request, _responseDict, userInfo, uploadFileSize, uploadBytesSent;
+// public properties
+@synthesize delegate, request, userInfo;
+// private properties
+@synthesize  _responseDict;
+@synthesize responseString = _responseString;
+@synthesize receivedData = _receivedData;
+@synthesize internalResponse = _internalResponse;
+@synthesize internalError = _internalError;
+@synthesize postValues = _postValues;
+@synthesize fileData = _fileData;
+@synthesize connection = _connection;
 
 - (id)initWithURL:(NSURL *)url {
     
     self = [super init];
     if (self) {
-        self.request = [ASIFormDataRequest requestWithURL:url];
-        self.request.postFormat = ASIMultipartFormDataPostFormat;
-        [self.request setPostValue:[ENAPI apiKey] forKey:@"api_key"];
-        self.request.delegate = self;
-        self.request.uploadProgressDelegate = self;
-        self.request.timeOutSeconds = 180;
+        self.request = [NSURLRequest requestWithURL:url];
+
+        [self.request setHTTPMethod:@"POST"];
+        [self.request setTimeoutInterval:180];
+        [self setPostValue:[ENAPI apiKey] forKey:@"api_key"];
     }
     return self;
 }
 
-- (void)setPostValue:(NSObject *)value forKey:(NSString *)key {
-    [self.request setPostValue:value forKey:key];
-}
-
-- (void)setFile:(NSString *)path forKey:(NSString *)key {
-    [self.request setFile:path forKey:key];
-}
-
-- (void)startSynchronous {
-    [self retain]; // let's make sure we're still around when the network call returns
-    [self.request startSynchronous];
-}
-
-- (void)startAsynchronous {
-    [self retain]; // let's make sure we're still around when the network call returns
-    [self.request startAsynchronous];
+- (void) dealloc {
+    delegate = nil;
+    [request release];
+    [_responseDict release];
+    [userInfo release];
+    [_responseString release];
+    [_internalError release];
+    [_internalResponse release];
+    [_postValues release];
+    [_fileData release];
+    [_connection release];
+    [super dealloc];
 }
 
 + (ENAPIPostRequest *)requestWithURL:(NSURL *)url {
@@ -89,9 +101,6 @@
     NSURL *url = [NSURL URLWithString:urlString];
     ENAPIPostRequest *postRequest = [ENAPIPostRequest requestWithURL:url];
     [postRequest setFile:filePath forKey:@"track"];
-    // get the upload file's size so we can report progress somewhat accurately
-    NSDictionary *fileAtts = [[NSFileManager defaultManager] attributesOfItemAtPath:filePath error:nil];
-    postRequest.uploadFileSize = [[fileAtts objectForKey:NSFileSize] unsignedLongLongValue];
     NSString *ext = [[filePath pathExtension] lowercaseString];
     [postRequest setPostValue:ext forKey:@"filetype"];
     return postRequest;
@@ -158,51 +167,178 @@
     return postRequest;            
 }
 
-#pragma mark - ASIProgressDelegate
-
-- (void)request:(ASIHTTPRequest *)request didSendBytes:(long long)bytes {
-    self.uploadBytesSent += bytes;
-    if ([self.delegate respondsToSelector:@selector(postRequest:didSendBytes:)]) {
-        [(id<ENAPIPostRequestDelegate>)self.delegate postRequest:self didSendBytes:bytes];
+- (void)setPostValue:(NSObject *)value forKey:(NSString *)key {
+    if (nil == _postValues) {
+        self.postValues = [[NSMutableDictionary alloc] initWithCapacity:10];
     }
-    if ([self.delegate respondsToSelector:@selector(postRequest:uploadProgress:)]) {
-        [(id<ENAPIPostRequestDelegate>)self.delegate postRequest:self uploadProgress:(float)self.uploadBytesSent/(float)self.uploadFileSize];
+    [self.postValues setObject:value forKey:key];
+}
+
+- (void)setFile:(NSString *)path forKey:(NSString *)key {
+    if (nil == _fileData) {
+        self.fileData = [[NSMutableDictionary alloc] initWithCapacity:5];
+    }
+    NSString *filename = [path lastPathComponent];
+    NSDictionary *fileInfo = [NSDictionary dictionaryWithObjectsAndKeys:path, @"path",
+                               filename, @"filename", key, @"key", nil];
+    [self.fileData setObject:fileInfo forKey:key];
+}
+
+- (void)startSynchronous {
+    [self retain]; // let's make sure we're still around when the network call returns
+
+    [self _preparePostBody];
+
+    NSData *data = [NSURLConnection sendSynchronousRequest:self.request
+                                         returningResponse:&_internalResponse
+                                                     error:&_internalError];
+
+    _responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+
+    if (nil == _internalError) {
+        [self _requestFinished];
+    } else {
+        [self _requestFailed];
     }
 }
 
-#pragma mark - ASIRequestDelegate
+- (void)startAsynchronous {
+    [self retain]; // let's make sure we're still around when the network call returns
 
-- (void)requestFinished:(ASIHTTPRequest *)request {
+    [self _preparePostBody];
+
+    self.connection = [NSURLConnection connectionWithRequest:self.request
+                                                    delegate:self];
+
+    // Schedule in the RunLoop
+    [self.connection start];
+}
+
+- (void)_prepareFormValuesForPostBody:(NSMutableData *)postBody withBoundary:(NSString *)boundary {
+    NSString *endBoundary = [NSString stringWithFormat:@"\r\n--%@\r\n",boundary];
+	NSUInteger idx=0;
+	for (NSString *key in [self.postValues allKeys]) {
+        NSString *valHeading = [NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n",key];
+		[postBody appendData:[valHeading dataUsingEncoding:NSUTF8StringEncoding]];
+		[postBody appendData:[[self.postValues objectForKey:key] dataUsingEncoding:NSUTF8StringEncoding]];
+
+        idx++;
+
+        // if processing value and it's not the last one, add delimiter
+		if (idx != [self.postValues count] || [self.fileData count] > 0) {
+			[postBody appendData:[endBoundary dataUsingEncoding:NSUTF8StringEncoding]];
+		}
+	}
+}
+
+- (void)_prepareFileDataForPostBody:(NSMutableData *)postBody withBoundary:(NSString *)boundary {
+    NSString *endBoundary = [NSString stringWithFormat:@"\r\n--%@\r\n",boundary];
+	NSUInteger idx=0;
+    for (NSDictionary *fileInfo in [self.fileData allValues]) {
+        NSString *valHeading = [NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n",
+                                [fileInfo objectForKey:@"key"], [fileInfo objectForKey:@"filename"]];
+        [postBody appendData:[valHeading dataUsingEncoding:NSUTF8StringEncoding]];
+        [postBody appendData:[NSData dataWithContentsOfFile:[fileInfo objectForKey:@"path"]]];
+
+        idx++;
+
+        // if processing value and it's not the last one, add delimiter
+        if (idx != [self.fileData count]) {
+            [postBody appendData:[endBoundary dataUsingEncoding:NSUTF8StringEncoding]];
+        }
+    }
+}
+
+- (void)_preparePostBody {
+    NSString *charset = (NSString *)CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+    NSString *boundary = @"---------------------0x520OlDPuEbLo19120214";
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; charset=%@; boundary=%@",charset,boundary];
+    [self.request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+
+    NSMutableData *postBody = [NSMutableData dataWithLength:0];
+
+    [self _prepareFormValuesForPostBody:postBody withBoundary:boundary];
+    [self _prepareFileDataForPostBody:postBody withBoundary:boundary];
+
+    NSLog(@"POST Body: \n%@", [[[NSString alloc] initWithData:postBody
+                                                     encoding:NSUTF8StringEncoding] autorelease]);
+
+    [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [request setHTTPBody:postBody];
+
+
+}
+
+#pragma mark - NSURLConnectionDelegate
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    self.internalResponse = (NSHTTPURLResponse *)response;
+    self.receivedData = [[[NSMutableData alloc] initWithLength:0] autorelease];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data_ {
+    [self.receivedData appendData:data_];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error_ {
+    self.internalError = error_;
+    [_receivedData release];
+    [self _requestFailed];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    _responseString = [[NSString alloc] initWithData:self.receivedData
+                                            encoding:NSUTF8StringEncoding];
+    [_receivedData release];
+    [self _requestFinished];
+}
+
+#pragma mark - NSURLConnectionDataDelegate
+
+
+- (void)connection:(NSURLConnection *)connection didSendBodyData:(NSInteger)bytesWritten
+                                               totalBytesWritten:(NSInteger)totalBytesWritten
+                                       totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite {
+    if ([self.delegate respondsToSelector:@selector(postRequest:didSendBytes:)]) {
+        [(id<ENAPIPostRequestDelegate>)self.delegate postRequest:self didSendBytes:bytesWritten];
+    }
+    if ([self.delegate respondsToSelector:@selector(postRequest:uploadProgress:)]) {
+        [(id<ENAPIPostRequestDelegate>)self.delegate postRequest:self uploadProgress:(float)totalBytesWritten/(float)totalBytesExpectedToWrite];
+    }
+}
+
+#pragma mark - Private Methods
+
+- (void)_requestFinished {
     if ([self.delegate respondsToSelector:@selector(postRequestFinished:)]) {
         [(id<ENAPIPostRequestDelegate>)self.delegate postRequestFinished:self];
     }
     [self release];
 }
 
-- (void)requestFailed:(ASIHTTPRequest *)request {
+- (void)_requestFailed {
     if ([self.delegate respondsToSelector:@selector(postRequestFailed:)]) {
         [(id<ENAPIPostRequestDelegate>)self.delegate postRequestFailed:self];
     }
     [self release];
 }
 
-
 #pragma mark - Properties
 
 - (NSDictionary *)response {
     if (nil == _responseDict) {
-        NSDictionary *dict = [self.request.responseString JSONValue];
+        NSDictionary *dict = [self.responseString JSONValue];
         _responseDict = [dict retain];
     }
     return _responseDict;
 }
 
 - (NSUInteger)responseStatusCode {
-    return self.request.responseStatusCode;
+    return [self.internalResponse statusCode];
 }
 
 - (NSError *)error {
-    return self.request.error;
+    return self.internalError;
 }
 
 - (NSUInteger)echonestStatusCode {
@@ -211,13 +347,5 @@
 
 - (NSString *)echonestStatusMessage {
     return [self.response valueForKeyPath:@"response.status.message"];
-}
-
-- (void) dealloc {
-    delegate = nil;
-    [request release];
-    [_responseDict release];
-    [userInfo release];
-    [super dealloc];
 }
 @end
